d6116dfbdd737fcab57f5b4cff0e5867f5a61892
Greg Haskins <greg@manetu.com>
2020-01-04 15:51:24 -0500

    Release v0.9.0
    Initial release of the library
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protojure/google.protobuf "0.1.0-SNAPSHOT"
+(defproject protojure/google.protobuf "0.9.0"
   :description "Protojure support for https://developers.google.com/protocol-buffers/docs/reference/google.protobuf"
   :url "https://github.com/protojure/google.protobuf"
   :license {:name "Apache License 2.0"
