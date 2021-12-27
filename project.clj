e6cfcac515705046fce05e5f8cdea5546a48fa4a
Gregory Haskins <greg@manetu.com>
2021-12-27 14:53:36 -0500

    Run lein-ancient (#4)
    * Run lein-ancient
    
    Signed-off-by: Greg Haskins <greg@manetu.com>
    
    * Update circleci config
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2020
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [protojure "1.1.0" :scope "provided"]
-                 [com.google.protobuf/protobuf-java "3.11.1" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 [protojure "1.7.0" :scope "provided"]
+                 [com.google.protobuf/protobuf-java "3.19.1" :scope "provided"]]
   :repl-options {:init-ns google.protobuf})
