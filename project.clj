3aed43e2b783dbca26273778b6cf224be1a60242
Greg Haskins <greg@manetu.com>
2021-12-27 15:07:37 -0500

    Prepare v1.0.1 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protojure/google.protobuf "1.0.0"
+(defproject protojure/google.protobuf "1.0.1-SNAPSHOT"
   :description "Protojure support for https://developers.google.com/protocol-buffers/docs/reference/google.protobuf"
   :url "https://github.com/protojure/google.protobuf"
   :license {:name "Apache License 2.0"
