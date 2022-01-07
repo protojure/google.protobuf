b51a818fe9f62e9005adb083cc7a4edfc74365d9
Greg Haskins <greg@manetu.com>
2022-01-07 10:00:35 -0500

    Release v2.0.0
    Changes since v1.0.0
    --------------------
    3ebb09d Update to protojure/core v2.0 (#5)
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject io.github.protojure/google.protobuf "2.0.0-SNAPSHOT"
+(defproject io.github.protojure/google.protobuf "2.0.0"
   :description "Protojure support for https://developers.google.com/protocol-buffers/docs/reference/google.protobuf"
   :url "https://github.com/protojure/google.protobuf"
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"
-            :year 2020
+            :year 2022
             :key "apache-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [io.github.protojure/core "2.0.1"]]
